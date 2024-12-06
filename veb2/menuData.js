7b8fe8d26dd69cc33d2b9f682e2fafc7c3dfe13f
ghost <158522322+nushuika@users.noreply.github.com>
2024-12-06 21:26:09 +0300

    Add files via upload

---
+++ veb2/menuData.js
@@ -0,0 +1,72 @@
+// menuData.js
+const dishes = [
+    { 
+        keyword: "gazpacho", 
+        name: "Гаспачо", 
+        price: 195, 
+        category: "soup", 
+        count: "350 г", 
+        image: "images/soup1.jpg" 
+    },
+    { 
+        keyword: "mushroom", 
+        name: "Грибной суп-пюре", 
+        price: 185, 
+        category: "soup", 
+        count: "330 г", 
+        image: "images/soup2.jpg" 
+    },
+    { 
+        keyword: "norwegian", 
+        name: "Норвежский суп", 
+        price: 270, 
+        category: "soup", 
+        count: "330 г", 
+        image: "images/soup3.jpg" 
+    },
+    { 
+        keyword: "fried-potatoes", 
+        name: "Жареная картошка с грибами", 
+        price: 150, category: "main", 
+        count: "250 г", 
+        image: "images/dish1.jpg" },
+    { 
+        keyword: "lasagna", 
+        name: "Лазанья", 
+        price: 385, 
+        category: "main", 
+        count: "310 г", 
+        image: "images/dish2.jpg" 
+    },
+    { 
+        keyword: "chicken-cutlets", 
+        name: "Котлеты из курицы с картофельным пюре", 
+        price: 225, 
+        category: "main", 
+        count: "280 г", 
+        image: "images/dish3.jpg" },
+    { 
+        keyword: "orange-juice", 
+        name: "Апельсиновый сок", 
+        price: 120, 
+        category: "drink", 
+        count: "300 мл", 
+        image: "images/juice1.jpg" 
+    },
+    { 
+        keyword: "apple-juice", 
+        name: "Яблочный сок", 
+        price: 90, 
+        category: "drink", 
+        count: "300 мл", 
+        image: "images/juice2.jpg" },
+    { 
+        keyword: "carrot-juice", 
+        name: "Морковный сок", 
+        price: 110, 
+        category: "drink", 
+        count: "300 мл", 
+        image: "images/juice3.jpg" 
+    },
+];
+export default dishes;
